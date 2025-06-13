a8de726f7fb105de4ebcf317c3340b285d0f6f75
𝕊ℙ𝕆𝕋𝕐 𝕄𝕋𝔽 <blaisedave2006@gmail.com>
2025-06-13 13:33:05 -0400

    Create utils.js

---
+++ commands/utils.js
@@ -0,0 +1,18 @@
+module.exports = {
+    isUrl: (text) => {
+        return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/gi));
+    },
+
+    getRandom: (ext) => {
+        return `${Math.floor(Math.random() * 10000)}${ext}`;
+    },
+
+    formatBytes: (bytes, decimals = 2) => {
+        if (bytes === 0) return '0 Bytes';
+        const k = 1024;
+        const dm = decimals < 0 ? 0 : decimals;
+        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+        const i = Math.floor(Math.log(bytes) / Math.log(k));
+        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+    }
+};
